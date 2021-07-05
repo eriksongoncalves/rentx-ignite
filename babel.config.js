b17d55319b2000611f34d1bb7e60cdadcb4983c6
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-07-05 16:20:46 -0300

    chore: config watermelondb

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['module:react-native-dotenv'], 'react-native-reanimated/plugin']
+    plugins: [
+      ['module:react-native-dotenv'],
+      'react-native-reanimated/plugin',
+      ['@babel/plugin-proposal-decorators', { legacy: true }]
+    ]
   };
 };
