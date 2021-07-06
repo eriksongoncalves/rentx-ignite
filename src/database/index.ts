390e583fe4fa0ae67df47442b94781e69914d200
Erikson Gonçalves <erikson.bezerra@amedigital.com>
2021-07-06 13:15:32 -0300

    feate: create table car

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,6 +3,7 @@ import SQLiteAdapter from '@nozbe/watermelondb/adapters/sqlite';
 
 import { schemas } from './schema';
 import { User } from './models/User';
+import { Car } from './models/Car';
 
 const adapter = new SQLiteAdapter({
   schema: schemas
@@ -10,6 +11,6 @@ const adapter = new SQLiteAdapter({
 
 export const database = new Database({
   adapter,
-  modelClasses: [User],
+  modelClasses: [User, Car],
   actionsEnabled: true
 });
--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,10 +1,11 @@
 import { appSchema } from '@nozbe/watermelondb/Schema';
 
 import { userSchema } from './userSchema';
+import { carSchema } from './carSchema';
 
 const schemas = appSchema({
-  version: 1,
-  tables: [userSchema]
+  version: 2,
+  tables: [userSchema, carSchema]
 });
 
 export { schemas };
